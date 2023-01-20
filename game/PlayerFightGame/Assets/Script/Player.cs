d0275b365abdcf2fb2c9403612c5b5c2516f709f
mickychin <chinkrittan@hotmail.com>
2023-01-20 18:26:38 +0700

    Try to make player look at

--- game/PlayerFightGame/Assets/Script/Player.cs
+++ game/PlayerFightGame/Assets/Script/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject Boss;
+    public Vector2 lookDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lookAtBoss();
+    }
+
+    void lookAtBoss()
+    {
+        lookDir = Boss.transform.position - transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x);
     }
 }
