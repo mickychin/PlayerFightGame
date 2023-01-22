5891dc0df05ed82cb2a5ec897eb5efcbcfa92520
Space-c2 <50313983+Space-c2@users.noreply.github.com>
2023-01-22 12:16:18 +0700

    BossAttack

---
+++ game/PlayerFightGame/Assets/Script/BossAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttack : MonoBehaviour
+{
+    [Header("Fireball")]
+    public KeyCode FireballKey;
+    public GameObject Fireball;
+
+    [Header("Firebreath")]
+    public KeyCode FirebreathKey;
+    public GameObject Firebreath;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(FireballKey))
+        {
+            Instantiate(Fireball,transform.position, transform.rotation);
+        }
+        if(Input.GetKeyDown(FirebreathKey))
+        {
+            Instantiate(Firebreath,transform.position, transform.rotation);
+        }
+    }
+}
--- game/PlayerFightGame/Assets/Script/Fireball.cs
+++ game/PlayerFightGame/Assets/Script/Fireball.cs
@@ -6,15 +6,20 @@ public class Fireball : MonoBehaviour
 {
     public float force = 10f;
     public Rigidbody2D RB2d;
-    // Start is called before the first frame update
+
     void Start()
     {
+        gameObject.transform.Rotate(90, 0, 0);
         RB2d.AddForce(transform.up * force, ForceMode2D.Impulse);  
     }
 
-    // Update is called once per frame
     void OnCollisionEnter2D()
     {
         Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        gameObject.transform.localScale += new Vector3(0.05f, 0.05f, 0.05f) * Time.deltaTime;
     }
 }
