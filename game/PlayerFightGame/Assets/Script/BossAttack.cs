974535e4de81e665781caca51d796e6b8012b189
mickychin <chinkrittan@hotmail.com>
2023-01-22 23:00:02 +0700

    lot of attack

--- game/PlayerFightGame/Assets/Script/BossAttack.cs
+++ game/PlayerFightGame/Assets/Script/BossAttack.cs
@@ -7,37 +7,59 @@ public class BossAttack : MonoBehaviour
     [Header("Fireball")]
     public KeyCode FireballKey;
     public GameObject Fireball;
+    bool CanFireBall = true;
 
     [Header("Firebreath")]
     public KeyCode FirebreathKey;
     public GameObject Firebreath;
+    bool canBreath = true;
 
     [Header("Firebeam")]
     public KeyCode FirebeamKey;
     public GameObject FirebeamCharge;
     public GameObject Firebeam;
+    bool canfireBeam = true;
 
     void Update()
     {
         //Debug.Log(this.gameObject.name);
-        if(Input.GetKeyDown(FireballKey))
+        if(Input.GetKeyDown(FireballKey) && CanFireBall)
         {
+            StartCoroutine(ShootFireBall());
             Instantiate(Fireball,transform.position, transform.rotation);
         }
-        if(Input.GetKeyDown(FirebreathKey))
+        if(Input.GetKeyDown(FirebreathKey) && canBreath)
         {
             Instantiate(Firebreath,transform.position, transform.rotation, gameObject.transform);
+            StartCoroutine(ShootBreath());
         }
-        if(Input.GetKeyDown(FirebeamKey))
+        if(Input.GetKeyDown(FirebeamKey) && canfireBeam)
         {
             Instantiate(FirebeamCharge,transform.position, transform.rotation, gameObject.transform);
             StartCoroutine(FirebeamCharging());
         }
     }
 
+    IEnumerator ShootFireBall()
+    {
+        CanFireBall = false;
+        yield return new WaitForSeconds(0.5f);
+        CanFireBall = true;
+    }
+
+    IEnumerator ShootBreath()
+    {
+        canBreath = false;
+        yield return new WaitForSeconds(10f);
+        canBreath = true;
+    }
+
     IEnumerator FirebeamCharging()
     {
+        canfireBeam = false;
         yield return new WaitForSeconds(2); // wait for 2 seconds
         Instantiate(Firebeam,transform.position, transform.rotation, gameObject.transform);
+        yield return new WaitForSeconds(10);
+        canfireBeam = true;
     }
 }
