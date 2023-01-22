1f4eede25814d5a64444850d4106a0d31702228a
mickychin <chinkrittan@hotmail.com>
2023-01-22 20:18:31 +0700

    Boss Anim Done

--- game/PlayerFightGame/Assets/Script/Boss.cs
+++ game/PlayerFightGame/Assets/Script/Boss.cs
@@ -9,12 +9,14 @@ public class Boss : MonoBehaviour
     public float rotateSpeed;
     public float speed;
     Rigidbody2D rb;
+    Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,6 +33,14 @@ public class Boss : MonoBehaviour
 
             Vector3 moveDir = new Vector3(horizontal, vertical).normalized;
             rb.velocity = moveDir * speed;
+            if(horizontal != 0 || vertical != 0)
+            {
+                animator.SetBool("walk", true);
+            }
+            else
+            {
+                animator.SetBool("walk", false);
+            }
         }
         if(canRotate == true)
         {
